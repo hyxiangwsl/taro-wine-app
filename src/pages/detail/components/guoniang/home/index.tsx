6ffd876025969ac81b32d656441a6ef6e7a2de6f
silence玉香 <18776181583@163.com>
2022-09-08 10:18:38 +0800

    update: 国酿背景图片切换

--- src/pages/detail/components/guoniang/home/index.tsx
+++ src/pages/detail/components/guoniang/home/index.tsx
@@ -2,11 +2,11 @@ import { View, Image, Text } from "@tarojs/components";
 import { Component } from "react";
 import { Back, Music, Next } from "@/pages/components";
 import "./index.less";
-import detail_logo from "../imgs/logo.png";
+import detail_logo from "../imgs/logo1.png";
+import detail_t from "../imgs/title1.png";
 import detail_product from "../imgs/product.png";
 
-
-export const Home =()=> {
+export const Home = () => {
   return (
     <View className='guoniang_home'>
       <View className='guoniang_home_header'>
@@ -15,7 +15,7 @@ export const Home =()=> {
       </View>
       <View className='guoniang_home_main fadeOut'>
         <Image src={detail_logo} className='guoniang_home_main_logo'></Image>
-        <Text className='guoniang_home_main_t'>酱香新高度</Text>
+        <Image src={detail_t} className='guoniang_home_main_t'></Image>
       </View>
       <View className='guoniang_home_pic_product scaleDraw'>
         <Image src={detail_product} className='guoniang_home_pic'></Image>
@@ -24,4 +24,4 @@ export const Home =()=> {
       <Next to='/pages/brands/index' />
     </View>
   );
-}
+};
