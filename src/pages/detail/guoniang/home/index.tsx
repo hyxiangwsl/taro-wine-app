c28cabf1e86ff20cfca072cfc9d8518fa12ab212
silence玉香 <18776181583@163.com>
2022-09-24 22:18:56 +0800

    update: 国酿

--- src/pages/detail/guoniang/home/index.tsx
+++ src/pages/detail/guoniang/home/index.tsx
@@ -1,10 +1,8 @@
 import { View, Image } from "@tarojs/components";
 import { Back, Music, Next } from "@/pages/components";
 import "./index.less";
-import detail_logo from "../imgs/logo1.png";
-import detail_t from "../imgs/title1.png";
-import detail_product from "../imgs/guoniang_product.png";
-import next from '../imgs/guoniang_next.png';
+import detail_logo from "./imgs/logo.png";
+import next from './imgs/guoniang_next.png';
 
 const Home = () => {
 
@@ -16,10 +14,6 @@ const Home = () => {
       </View>
       <View className='guoniang_home_main fadeOut'>
         <Image src={detail_logo} className='guoniang_home_main_logo'></Image>
-        <Image src={detail_t} className='guoniang_home_main_t'></Image>
-      </View>
-      <View className='guoniang_home_pic_product scaleDraw'>
-        <Image src={detail_product} className='guoniang_home_pic'></Image>
       </View>
       {/* 跳转下一页 */}
       <Next className='guoniang_home_next' img={next} />
