5b1fd99e8a13ffe22ef511708280ac2b7e9bd2ab
silence玉香 <18776181583@163.com>
2022-09-25 22:52:13 +0800

    update: 雪域

--- src/pages/detail/xueyu/home/index.tsx
+++ src/pages/detail/xueyu/home/index.tsx
@@ -7,7 +7,6 @@ import { contextSlice } from "@/redux/contextSlice";
 import "./index.less";
 import detail_logo from "./imgs/xueyu_logo.png";
 import detail_logo1 from "./imgs/xueyu_logo2.png";
-import detail_product from "./imgs/xueyu_product.png";
 import detail_l from "./imgs/xueyu_detail_l.png";
 import detail_r from "./imgs/xueyu_detail_r.png";
 import xueyu_brand_desc from "./imgs/xueyu_brand_desc.png";
@@ -15,7 +14,6 @@ import xueyu_brand_desc from "./imgs/xueyu_brand_desc.png";
 const Home = () => {
   const [isPortalVisible, setIsPortalVisible] = useState(false);
 
-  const [isShow, setIsShow] = useState(false);
 
   const [isShowBtn, setIsShowBtn] = useState(false);
 
@@ -37,11 +35,8 @@ const Home = () => {
 
   useEffect(() => {
     setTimeout(() => {
-      setIsShow(true);
-    }, 2000);
-    setTimeout(() => {
       setIsShowBtn(true);
-    }, 3000);
+    }, 2000);
   }, []);
 
   return (
@@ -50,29 +45,24 @@ const Home = () => {
         <Back />
         <Music />
       </View>
-      <View className='xueyu_home_main' style={{ opacity: isShow ? 1 : 0 }}>
-        <Image src={detail_logo} className='xueyu_home_main_logo'></Image>
-        <Image src={detail_logo1} className='xueyu_home_main_logo2'></Image>
+      <View className='xueyu_home_main' >
+        <Image src={detail_logo} className='xueyu_home_main_logo fadeOut'></Image>
       </View>
       <View className='xueyu_home_pic_product'>
-        <Image
-          src={detail_product}
-          className='xueyu_home_pic scaleDraw'
-        ></Image>
         <View
           className='footer_detail_con'
           style={{ opacity: isShowBtn ? 1 : 0 }}
         >
-          <Image
-            src={detail_l}
-            className='footer_card'
-            onClick={toNextPage}
-          ></Image>
           {/* 产品介绍 */}
           <Image
             src={detail_r}
             className='footer_card'
             onClick={showPortal}
+          ></Image>
+          <Image
+            src={detail_l}
+            className='footer_card'
+            onClick={toNextPage}
           ></Image>
         </View>
       </View>
