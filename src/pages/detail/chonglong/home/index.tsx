b45eb6f46e49447bf4a28bc8f55371d32ed7f62a
silence玉香 <18776181583@163.com>
2022-09-25 22:35:18 +0800

    update:重龙

--- src/pages/detail/chonglong/home/index.tsx
+++ src/pages/detail/chonglong/home/index.tsx
@@ -6,16 +6,12 @@ import { useDispatch } from "@/redux/hooks";
 import { contextSlice } from "@/redux/contextSlice";
 import "./index.less";
 import detail_logo from "./imgs/chonglong_logo.png";
-import detail_logo1 from "./imgs/chonglong_logo2.png";
-import detail_product from "./imgs/chonglong_product.png";
 import detail_l from "./imgs/chonglong_detail_l.png";
 import detail_r from "./imgs/chonglong_detail_r.png";
 import chonglong_brand_desc from "./imgs/chonglong_brand_desc.png";
 
 const Home = () => {
   const [isPortalVisible, setIsPortalVisible] = useState(false);
-
-  const [isShow, setIsShow] = useState(false);
 
   const [isShowBtn, setIsShowBtn] = useState(false);
 
@@ -37,11 +33,8 @@ const Home = () => {
 
   useEffect(() => {
     setTimeout(() => {
-      setIsShow(true);
-    }, 2000);
-    setTimeout(() => {
       setIsShowBtn(true);
-    }, 3000);
+    }, 2000);
   }, []);
 
   return (
@@ -50,29 +43,24 @@ const Home = () => {
         <Back />
         <Music />
       </View>
-      <View className='chonglong_home_main' style={{ opacity: isShow ? 1 : 0 }}>
-        <Image src={detail_logo} className='chonglong_home_main_logo'></Image>
-        <Image src={detail_logo1} className='chonglong_home_main_logo2'></Image>
+      <View className='chonglong_home_main' >
+        <Image src={detail_logo} className='chonglong_home_main_logo fadeOut'></Image>
       </View>
       <View className='chonglong_home_pic_product'>
-        <Image
-          src={detail_product}
-          className='chonglong_home_pic scaleDraw'
-        ></Image>
         <View
           className='footer_detail_con'
           style={{ opacity: isShowBtn ? 1 : 0 }}
         >
-          <Image
-            src={detail_l}
-            className='footer_card'
-            onClick={toNextPage}
-          ></Image>
           {/* 产品介绍 */}
           <Image
             src={detail_r}
             className='footer_card'
             onClick={showPortal}
+          ></Image>
+          <Image
+            src={detail_l}
+            className='footer_card'
+            onClick={toNextPage}
           ></Image>
         </View>
       </View>
